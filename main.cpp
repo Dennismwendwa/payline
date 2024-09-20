9cb2fc49744d72e9659532599c94dfb66bed2b92
Dennismwendwa <dennismusembi2@gmail.com>
2024-09-20 04:41:45 +0300

    added class definations in main.h

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include "main.h"
 
-int main()
-{
-    std::cout << "starting the app!\n";
+int main() {
+    User* users[3];
+    users[0] = new Cashier("john_doe", "password123", "john@example.com", "John", "Doe");
+    users[1] = new Supervisor("jane_smith", "password456", "jane@example.com", "Jane", "Smith");
+    users[2] = new Manager("admin_user", "password789", "admin@example.com", "Admin", "User");
+
+    for (int i = 0; i < 3; ++i) {
+        users[i]->displayRole();
+        std::cout << std::endl;
+        delete users[i]; // Clean up
+    }
 
     return 0;
 }
--- main.h
+++ main.h
@@ -4,4 +4,52 @@
 #include <iostream>
 #include <vector>
 
+class User
+{
+    std::string user_type = "normal";
+
+    protected:
+        std::string first_name;
+        std::string last_name;
+        std::string username;
+        std::string password;
+        std::string email;
+
+    public:
+        User(const std::string &user, const std::string &pass,
+        const std::string &email, const std::string first_name,
+        const std::string &last_name);
+
+        virtual void displayRole() const;
+        virtual ~User();
+};
+
+class Cashier : public User {
+public:
+    Cashier(const std::string& user, const std::string& pass,
+            const std::string& email, const std::string& first_name,
+            const std::string& last_name);
+
+    void displayRole() const override;
+};
+
+class Supervisor : public User {
+public:
+    Supervisor(const std::string& user, const std::string& pass,
+               const std::string& email,
+               const std::string& firstName, const std::string& lastName);
+
+    void displayRole() const override;
+};
+
+// Derived class for Manager
+class Manager : public User {
+public:
+    Manager(const std::string& user, const std::string& pass,
+            const std::string& email, const std::string& firstName,
+            const std::string& lastName);
+
+    void displayRole() const override;
+};
+
 #endif
